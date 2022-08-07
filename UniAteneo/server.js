7c5e6b4622d150640cbe65fe5f85ae77f4deefbd
Marco Schivo <marcoschivo1@gmail.com>
2022-08-07 13:17:32 +0200

    adding sqlite

--- UniAteneo/server.js
+++ UniAteneo/server.js
@@ -1,8 +1,26 @@
-'use strict';
-var http = require('http');
-var port = process.env.PORT || 1337;
+const sqljs = require('sql.js');
+const http = require('http');
+const fs = require('fs');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+const filebuffer = fs.readFileSync('./data/db.sqlite');
+
+const web_port = process.env.PORT || 1337;
+
+sqljs().then(function (SQL) {
+    // Load the db
+    const db = new SQL.Database(filebuffer);
+});
+
+const server = http.createServer(function (req, res) {
+    res.writeHead(200);
+    res.end('Hello, World!');
+});
+
+server.listen(web_port);
+
+
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Process terminated');
+    });
+});
