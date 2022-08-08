bbfcbf940bc0ff70560af2682fac42a38a75954f
Marco Schivo <marcoschivo1@gmail.com>
2022-08-08 16:45:38 +0200

    implementing express

--- UniAteneo/server.js
+++ UniAteneo/server.js
@@ -1,51 +1,38 @@
+const express = require('express');
 const sql_js = require('sql.js');
-const http = require('http');
 const fs = require('fs');
 
 const path_db = "./data/db.sqlite";
 const path_init_db = "./data/init_db.sql";
 const web_port = process.env.PORT || 1337;
 
-//const SQL = await sql_js.Database;
-//var db;
+const server = express();
 
-try {
-    // create the database
-    if (fs.existsSync(path_db)) {
-        console.log("asd");
-        const file_buffer = fs.readFileSync(path_db);
-        //sql_js().then(function (SQL) {
-        //    // load the db
-        //    db = new SQL.Database(file_buffer);
-        //});
-    } else {
-        const sql_string = fs.readFileSync(path_init_db, { encoding: 'utf8', flag: 'r' });
-        //sql_js().then(function (SQL) {
-        //    // load the db
-        //    db = new SQL.Database();
-        //});
-        db = new sql_js.Database();
+// create the database if it doesn't exist
+if (!fs.existsSync(path_db)) {
+    const sql_string = fs.readFileSync(path_init_db, { encoding: 'utf8', flag: 'r' });
+    sql_js().then(function (SQL) {
+        const db = new SQL.Database();
         db.exec(sql_string);
-    }
-} catch (err) {
-    console.log(err);
+        fs.writeFileSync(path_db, Buffer.from(db.export()));
+        console.log("The SQLite database has been initiated and stored in '" +
+                    path_db + 
+                    "'.")
+        db.close();
+    });
 }
 
-// create the web server
-const server = http.createServer(function (req, res) {
-    res.writeHead(200);
-    res.end('Hello, World!');
-});
-server.listen(web_port);
+server.get('/', (req, res) => {
+    res.send('Hello World!')
+})
 
-process.on('SIGTERM', () => {
-    // closing the web server
-    server.close(() => {
-        console.log('web server terminated');
-    });
-    // exporting the database
-    const data = db.export();
-    const buffer = Buffer.from(data);
-    fs.writeFileSync(path_db, buffer);
-    db.close();
-});
+server.listen(web_port, () => {
+    console.log(`web server is listeing to port: ${web_port}.`)
+})
+
+//process.on('SIGTERM', () => {
+//    // closing the web server
+//    server.close(() => {
+//        console.log('web server terminated.');
+//    });
+//});
