b33d5f92aa0ef2df504cc36f54dce0954f437aaf
Marco Schivo <marcoschivo1@gmail.com>
2022-08-26 14:53:54 +0200

    mod create tables sql

--- UniAteneo/data/sql/create_tables.sql
+++ UniAteneo/data/sql/create_tables.sql
@@ -40,32 +40,41 @@ CREATE TABLE IF NOT EXISTS Programmi (
 );
 
 CREATE TABLE IF NOT EXISTS Studente (
-	matricola   INTEGER IDENTITY(6000,15) PRIMARY KEY,
+	matricola   INTEGER,
 	nome        tinytext,
 	cognome     tinytext,
 	reddito     INTEGER,
 	anno        tinyINTEGER,     -- 1,2,3,4 OR 5
 	rate_pagate tinyINTEGER,	 -- 0,1 OR 2
-	id_corso    INTEGER FOREIGN KEY REFERENCES CDS(id)
+	id_corso    INTEGER,
+	PRIMARY KEY (matricola),
+	FOREIGN KEY (id_corso) REFERENCES CDS(id)
 );
 
 CREATE TABLE IF NOT EXISTS PianoDiStudi (
-	matricola       INTEGER FOREIGN KEY REFERENCES Studente(matricola),
-	id_insegnamento INTEGER FOREIGN KEY REFERENCES Insegnamenti(id),
+	matricola       INTEGER,
+	id_insegnamento INTEGER,
 	voto            tinytext  DEFAULT '0',
-	PRIMARY KEY (matricola, id_insegnamento)
+	PRIMARY KEY (matricola, id_insegnamento),
+	FOREIGN KEY (matricola) 	  REFERENCES Studente(matricola),
+	FOREIGN KEY (id_insegnamento) REFERENCES Insegnamenti(id)
 );
 
 CREATE TABLE IF NOT EXISTS Esame (
-	id              INTEGER IDENTITY(100,15) PRIMARY KEY,
-	id_insegnamento INTEGER FOREIGN KEY REFERENCES Insegnamenti(id),
-	id_corso        INTEGER FOREIGN KEY REFERENCES CDS(id),
-	data            datetime
+	id              INTEGER,
+	id_insegnamento INTEGER,
+	id_corso        INTEGER,
+	data            datetime,
+	PRIMARY KEY (id),
+	FOREIGN KEY (id_insegnamento) REFERENCES Insegnamenti(id),
+	FOREIGN KEY (id_corso) 		  REFERENCES CDS(id)
 );
 
 CREATE TABLE IF NOT EXISTS InscrizioniEsami (
-	matricola       INTEGER FOREIGN KEY REFERENCES Studente(matricola),
-	id_esame        INTEGER FOREIGN KEY REFERENCES Esame(id),
+	matricola       INTEGER,
+	id_esame        INTEGER,
 	data_iscrizione datetime,
-	PRIMARY KEY (matricola, id_esame)
+	PRIMARY KEY (matricola, id_esame),
+	FOREIGN KEY (matricola) REFERENCES Studente(matricola),
+	FOREIGN KEY (id_esame)  REFERENCES Esame(id)
 );
